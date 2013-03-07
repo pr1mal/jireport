a42d52ab3983bf2af96c3fbb10656a807716d95a
Evgeny Sokolov <e.sokolov@gmail.com>
2013-03-07 11:28:20 +0300

    Moved styles to document metadata, XLSX export minor refactoring
    Refactored document styling:
    - move style rescription to document metadata (under :style key)
    - disable style caching, otherwise there was no ability to have
    different doc styles on different sheets
    
    TODO move report description to config file

--- lib/jireport/export_xlsx.rb
+++ lib/jireport/export_xlsx.rb
@@ -3,35 +3,6 @@ require 'pp'
 
 module JiReport
   class ExportXlsx
-
-    STYLE_CONFIG = {
-      alignment: {
-        alignment: {
-          horizontal: :center,
-          vertical: :center,
-          wrap_text: true
-        }
-      },
-      data_alignment: {
-        alignment: {
-          horizontal: :left,
-          vertical: :center,
-          wrap_text: true
-        }
-      },
-      header_color: {
-        bg_color: 'fefe98',
-        b: true,
-        locked: true,
-        border: { style: :thin, color: '00'}
-      },
-      username_color: {
-        fg_color: 'ff0000',
-        b: true,
-        locked: true
-      }
-    }
-
     def initialize(metadata, data)
       metadata.each do |sheet_meta|
         next unless sheet_meta[:columns]
@@ -40,7 +11,7 @@ module JiReport
           # generate header
           row_data = sheet_meta[:columns].map {|c| c[:header] || c[:source].last.to_s }
           col_widths = sheet_meta[:columns].map {|c| c[:width] || :ignore }
-          s.add_row(row_data, {height: 25, style: header_style, widths: col_widths})
+          s.add_row(row_data, {height: 25, style: header_style(sheet_meta[:style]), widths: col_widths})
           
           # fill the data
           username_meth = sheet_meta[:columns].find {|c| cs = c[:source]; cs && cs.first.equal?(:user) }[:source].last
@@ -50,20 +21,26 @@ module JiReport
 
           data.each do |user, issues|
             user_data = [user.send(username_meth)]
-            user_styles = [username_style]
+            user_styles = [username_style(sheet_meta[:style])]
             if sheet_meta[:no_data]
               sheet_meta[:columns].each do |c|
                 next if c[:header] == "Name" # there is already username
                 user_data << c[:value] || ""
                 user_styles << nil # we don't want to style other cells
               end
+            elsif style_has_keys?(sheet_meta[:style][:username_color], :full_row) # apply style to the whole row, not just one cell
+              sheet_meta[:columns].each do |c|
+                next if c[:header] == "Name" # there is already username
+                user_data << ""
+              end
+              user_styles = user_styles.first
             end
             s.add_row user_data, { style: user_styles, widths: col_widths }
 
             unless sheet_meta[:no_data]
               issues.each do |issue|
                 s.add_row(["", "", issue_meths.map {|method| issue.send(method) }].flatten,
-                          { style: data_style, widths: col_widths })
+                          { style: data_style(sheet_meta[:style]), widths: col_widths })
               end
 
               s.add_row [] # add empty row
@@ -90,16 +67,24 @@ module JiReport
       workbook.styles.add_style(*args)
     end
     
-    def header_style
-      @header_style ||= add_style(STYLE_CONFIG[:header_color].merge(STYLE_CONFIG[:alignment]))
+    def header_style(style)
+      return nil unless style_has_keys?(style, :header_color, :header_alignment)
+      add_style((style[:header_color] || {}).merge(style[:header_alignment] || {}))
     end
 
-    def username_style
-      @username_style ||= add_style(STYLE_CONFIG[:username_color])
+    def username_style(style)
+      return nil unless style_has_keys?(style, :username_color)
+      add_style(style[:username_color])
     end
 
-    def data_style
-      @data_style ||= add_style(STYLE_CONFIG[:data_alignment])
+    def data_style(style)
+      return nil unless style_has_keys?(style, :data_alignment)
+      add_style(style[:data_alignment])
+    end
+
+    def style_has_keys?(style, *keys)
+      return false unless (style and style.is_a?(Hash) and keys)
+      return !(keys.find {|k| style.has_key?(k) }.nil?)
     end
   end
 end

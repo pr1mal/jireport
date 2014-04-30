6c0b084600865b229fa9642dc30b2ed0b00f769c
Artem Sheremet <dot.doom@gmail.com>
2014-04-29 20:35:41 -0700

    Rails 3 -> 4

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Jireport::Application.config.secret_token = '053549fcf61325ee21f0d5e72ee014a8d6a1cf48368854ae7907f5e0510e1323fe781ff0656e5b011c360f1a20a31105341dfdb6e5198b67aa555fc9ad2d85b0'
